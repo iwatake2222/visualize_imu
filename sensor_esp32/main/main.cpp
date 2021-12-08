0020225da96dd58e7eba9ce344b0164451bede52
iwatake2222 <take.iwiw2222@gmail.com>
2021-12-08 18:41:10 +0900

    [sensor_esp32] Implement Simple UDP

---
+++ sensor_esp32/main/main.cpp
@@ -0,0 +1,60 @@
+/* Copyright 2021 iwatake2222
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+/*** Include ***/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "asio.hpp"
+
+#include "wifi_helper.h"
+
+/*** Macro ***/
+static const char *TAG = "main";
+static const char *ADDRESS_DST = "192.168.1.2";
+static uint16_t PORT_DST = 1234;
+
+/*** Global variable ***/
+
+/*** Function ***/
+extern "C" void app_main(void)
+{
+    ESP_LOGI(TAG, "Initialize NVS");
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    // ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_LOGI(TAG, "Initialize WiFi as Station mode");
+    wifi_init_sta();
+
+    ESP_LOGI(TAG, "Create UDP socket to send");
+    asio::io_context io_context;
+    asio::ip::udp::socket socket(io_context, asio::ip::udp::endpoint(asio::ip::udp::v4(), 1234));
+    asio::ip::udp::endpoint destination(asio::ip::address_v4::from_string(ADDRESS_DST), PORT_DST);
+
+    while(1) {
+        uint32_t current_ms_time = pdTICKS_TO_MS(xTaskGetTickCount());
+        printf("%d\n", current_ms_time);
+        char buffer[128];
+        snprintf(buffer, sizeof(buffer), "[%08d] Message from ESP32", current_ms_time);
+        int32_t msg_len = strnlen(buffer, sizeof(buffer));
+        socket.send_to(asio::buffer(buffer, msg_len), destination);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
