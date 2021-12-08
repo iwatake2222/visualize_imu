683beff8b81d58bb73c772724cc6b0887f2e2952
iwatake2222 <take.iwiw2222@gmail.com>
2021-12-08 15:44:16 +0900

    Add blink test program

--- sensor_esp32/main/main.c
+++ sensor_esp32/main/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#define BLINK_GPIO 2
+
 void app_main(void)
 {
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+        printf("Turning off the LED\n");
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
 
+        printf("Turning on the LED\n");
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
