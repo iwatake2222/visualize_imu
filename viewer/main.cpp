41e4f97c1c9cdd176addf582626f9b9046e8945c
iwatake2222 <take.iwiw2222@gmail.com>
2021-12-14 19:37:23 +0900

    Simple imu calculation

--- viewer/main.cpp
+++ viewer/main.cpp
@@ -43,6 +43,7 @@ LsmSphere mag_calibrator;
 
 /*** Function ***/
 static inline float Deg2Rad(float deg) { return static_cast<float>(deg * M_PI / 180.0); }
+static inline float Rad2Deg(float rad) { return static_cast<float>(rad * 180.0 / M_PI); }
 
 static std::string GetDateTimeText()
 {
@@ -90,6 +91,42 @@ static void ThreadReceiver()
     }
 }
 
+class ObjectImu
+{
+public:
+    ObjectImu() {};
+    void Calculate(ValImu val_imu, float& pitch, float& yaw, float& roll, float& x, float& y, float& z)
+    {
+        float acc_x = val_imu.acc[0];
+        float acc_y = val_imu.acc[2];
+        float acc_z = val_imu.acc[1];
+        float gyro_x = val_imu.gyro[0];
+        float gyro_y = val_imu.gyro[2];
+        float gyro_z = val_imu.gyro[1];
+        float mag_x = val_imu.mag[0];
+        float mag_y = val_imu.mag[2];
+        float mag_z = val_imu.mag[1];
+        pitch = 0;
+        yaw = 0;
+        roll = 0;
+        x = 0;
+        y = 0;
+        z = 0;
+
+        pitch = atan2(-acc_y, -acc_z);
+        roll = atan2(-acc_x, sqrt(acc_y * acc_y + acc_z * acc_z));
+
+        //float Dx = cos(pitch) * mag_x + sin(pitch) * sin(roll) * mag_y + sin(pitch) * cos(roll) * mag_z;
+        //float Dy = cos(roll)* mag_y - sin(roll) * mag_z;
+        //float Dz = -sin(pitch) * mag_x + cos(pitch) * sin(roll) * mag_y + cos(pitch) * cos(roll) * mag_z;
+        //yaw = atan2(-Dy, Dx);
+
+        pitch = Rad2Deg(pitch);
+        yaw = Rad2Deg(yaw);
+        roll = Rad2Deg(roll);
+
+    }
+};
 
 int main(int argc, char* argv[])
 {
@@ -97,6 +134,7 @@ int main(int argc, char* argv[])
 
     GraphScatterImu graph_scatter(ACC_FS, GYRO_FS, MAG_FS / 2.0);
     GraphObject graph_object("Object", 5);
+    ObjectImu object_imu;
 
     std::deque<ValImu> val_imu_list;    /* Store history for scatter graph */
 
@@ -141,9 +179,14 @@ int main(int argc, char* argv[])
         graph_scatter.Update(key, acc_list, gyro_list, mag_list);
 
         /*** Draw Object ***/
-        static float deg = 0;
-        if (++deg >= 360) deg = 0;
-        graph_object.Update(key, deg, deg, deg, 0.5, 0.5, -0.5);
+        float pitch;
+        float yaw;
+        float roll;
+        float x;
+        float y;
+        float z;
+        object_imu.Calculate(val_imu_latest, pitch, yaw, roll, x, y, z);
+        graph_object.Update(key, pitch, yaw, roll, x, y, z);
     }
 
     do_exit = true;
